3336221717051bdd921e0bfc366e38eae4b1742e
Shimabukuro Sora <s23012@std.it-college.ac.jp>
2024-01-23 07:45:11 +0000

    chapter2.9終了 indexにimageOn属性を追加し、heroコンポーネントでトップページ以外は画像をオンにしないためにimageOnの初期値をfalseに指定した。

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Hero from 'components/hero'
 const Home = () => {
   const props1 = { title: 'CUBE', subtitle: 'アウトプットしていくサイト' }
-  return <Hero {...props1} />
+  return <Hero {...props1} imageOn />
 }
 
 export default Home
