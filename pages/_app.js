47c99b116e15beb606f5b94e83e381bebf5ce028
Shimabukuro Sora <s23012@std.it-college.ac.jp>
2024-01-24 06:14:25 +0000

    chapter3.3終了 globals.cssに基本設定とリセットの設定を追加しグローバルスタイルを設定し余白などをなくした。

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
+import 'styles/globals.css'
 import Layout from 'components/layout'
-function MyApp ({ Component, pageProps }) {
+const MyApp = ({ Component, pageProps }) => {
   return (
     <Layout>
       <Component {...pageProps} />
