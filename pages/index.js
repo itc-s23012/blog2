c7ed99a23ba0bd10ddee92d0a70918325b8a8fbf
Shimabukuro Sora <s23012@std.it-college.ac.jp>
2024-01-30 07:51:40 +0000

    chapter6.4終了 constant.jsにsiteMetaオブジェクトで情報を記述しmeta.jsでsiteMetaをインポートし使いたい情報を取り出しタイトルを|で区切る形にした。さらに、pageTitle属性でページのタイトルが未指定な場合の処理を指定した。

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero'
 const Home = () => {
   const props1 = { title: 'CUBE', subtitle: 'アウトプットしていくサイト' }
   return (
     <Container>
+      <Meta />
       <Hero {...props1} imageOn />
     </Container>
   )
