34392b7ff6a108b45f06e40e3aa4599dcbea26e2
Shimabukuro Sora <s23012@std.it-college.ac.jp>
2024-02-01 06:44:28 +0000

    chapter8.4終了 記事の本文を表示させ変換処理を行うコンポーネントを作成しそれを使い<PostBody>でスタイルを指定し見出しや段落の間隔を整えた。

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,6 +1,13 @@
 import { getPostBySlug } from 'lib/api'
 import Container from 'components/container'
 import PostHeader from 'components/post-header'
+import PostBody from 'components/post-body'
+import {
+  TwoColumn,
+  TwoColumnMain,
+  TwoColumnSidebar
+} from 'components/two-column'
+import ConvertBody from 'components/convert-body'
 import Image from 'next/legacy/image'
 
 const Schedule = ({ title, publish, content, eyecatch, categories }) => {
@@ -19,13 +26,21 @@ const Schedule = ({ title, publish, content, eyecatch, categories }) => {
             priority
           />
         </figure>
+        <TwoColumn>
+          <TwoColumnMain>
+            <PostBody>
+              <ConvertBody contentHTML={content} />
+            </PostBody>
+          </TwoColumnMain>
+          <TwoColumnSidebar></TwoColumnSidebar>
+        </TwoColumn>
       </article>
     </Container>
   )
 }
 
 const getStaticProps = async () => {
-  const slug = 'schedule'
+  const slug = 'music'
 
   const post = await getPostBySlug(slug)
 
