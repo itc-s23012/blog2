e93344ac08df49868498136268a10c10610494ff
Shimabukuro Sora <s23012@std.it-college.ac.jp>
2024-02-06 07:06:01 +0000

    chapter9.2終了 schedule.jsから[slug].jsnに名前を変更しそこにgetStaticPathsを追加し生成するページのスラッグを渡すためのURLを指定した。それをgetStaticPropsで受取記事ページを生成した。

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -15,7 +15,7 @@ import Image from 'next/legacy/image'
 import { getPlaiceholder } from 'plaiceholder'
 import { eyecatchLocal } from 'lib/constants'
 
-const Schedule = ({
+const Post = ({
   title,
   publish,
   content,
@@ -62,8 +62,15 @@ const Schedule = ({
   )
 }
 
-const getStaticProps = async () => {
-  const slug = 'micro'
+const getStaticPaths = async () => {
+  return {
+    paths: ['/blog/schedule', '/blog/music', '/blog/micro'],
+    fallback: false
+  }
+}
+
+const getStaticProps = async context => {
+  const slug = context.params.slug
 
   const post = await getPostBySlug(slug)
 
@@ -86,5 +93,6 @@ const getStaticProps = async () => {
   }
 }
 
-export default Schedule
+export default Post
+export { getStaticPaths }
 export { getStaticProps }
