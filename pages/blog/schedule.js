379d12552f4b6e4a781ecffecfeadffe0ba7a1cd
Shimabukuro Sora <s23012@std.it-college.ac.jp>
2024-02-06 06:49:04 +0000

    chapter9.1終了 post.eyecatchと同じ構造のeyecatchLocalを追加しURL,横幅、高さを指定しscheduleにインポートしてローカルの代替画像が表示されるようにした。更にPlaiceholderをインストールしプレースホルダーのブラー画像を設定した。

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -12,6 +12,8 @@ import {
 import ConvertBody from 'components/convert-body'
 import PostCategories from 'components/post-categories'
 import Image from 'next/legacy/image'
+import { getPlaiceholder } from 'plaiceholder'
+import { eyecatchLocal } from 'lib/constants'
 
 const Schedule = ({
   title,
@@ -41,6 +43,8 @@ const Schedule = ({
             height={eyecatch.height}
             sizes='(min-width: 1152px) 1152px, 100vw'
             priority
+            placeholder='blur'
+            blurDataURL={eyecatch.blurDataURL}
           />
         </figure>
         <TwoColumn>
@@ -59,18 +63,23 @@ const Schedule = ({
 }
 
 const getStaticProps = async () => {
-  const slug = 'music'
+  const slug = 'micro'
 
   const post = await getPostBySlug(slug)
 
   const description = extractText(post.content)
+
+  const eyecatch = post.eyecatch ?? eyecatchLocal
+
+  const { base64 } = await getPlaiceholder(eyecatch.url)
+  eyecatch.blurDataURL = base64
 
   return {
     props: {
       title: post.title,
       publish: post.publishDate,
       content: post.content,
-      eyecatch: post.eyecatch,
+      eyecatch,
       categories: post.categories,
       description
     }
